e453992ea95b0176664dba5c11320461a659c488
serajshaikh <serazshaikh007@gmail.com>
2022-10-15 16:59:02 +0530

    SignIn form authentication completed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
+  signInForm!: FormGroup
+  constructor(private formbuilder: FormBuilder, private http: HttpClient, private routes: Router) { }
   ngOnInit(): void {
+    this.signInForm = this.formbuilder.group({
+      name: [""],
+      pass: [""]
+    });
+
+  }
+  signIn() {
+    this.http.get<any>('http://localhost:3000/user').subscribe((res) => {
+      const user=res.find((a:any)=>{
+          // console.log(a);
+          // console.log(a.pass);
+          return a.name==this.signInForm.value.name && a.pass==this.signInForm.value.pass;
+      })
+      if(user){
+        alert("Thanks For Subscribing my Service");
+        this.signInForm.reset;
+      }
+      else{
+        alert("Sahi password dalo yaar!!😎")
+      }
+    })
   }
 
 }
